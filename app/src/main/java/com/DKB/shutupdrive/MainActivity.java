594bf9b1b6e91dbef26555379448c0f7e0ee578c
kylecorry31 <kylecorry31@gmail.com>
2016-02-28 18:52:53 -0500

    Remove CarMode called on start

--- app/src/main/java/com/DKB/shutupdrive/MainActivity.java
+++ app/src/main/java/com/DKB/shutupdrive/MainActivity.java
@@ -64,7 +64,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         running = Utils.isRunning(this);
         toast = false;
         buildGoogleApiClient();
-        startService(new Intent(this, CarMode.class));
     }
 
     private void createAds() {
